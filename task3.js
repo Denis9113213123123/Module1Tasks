cdd6607b9d53cc03ddca36b8698f56fc48f8e286
Denis <denis911p@gmail.com>
2023-01-04 23:38:33 +0300

    task4-completed

--- task3.js
+++
@@ -1,8 +0,0 @@
-const array1 = ["One", "Two", "Three", "Four", "Five"];
-let i = 0;
-const separationArray = (arr) => {
-  while (i < arr.length) {
-    console.log(arr.pop());
-  }
-};
-separationArray(array1);

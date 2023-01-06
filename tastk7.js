21c8cbf16c970792bb0eb37e09d36e119d1a7460
Denis <denis911p@gmail.com>
2023-01-06 15:54:19 +0300

    task7-fix

--- tastk7.js
+++ tastk7.js
@@ -1,7 +1,14 @@
-const array1 = [5,10,15,20,10,25,20,30,15];
+const array1 = [5, 10, 15, 20, 10, 25, 20, 30, 15];
 const N = 55;
-const sum = 0;
 const funct = (arr) => {
-
+  let sum = 0;
+  let i = 0
+  while (sum <= N) {
+    sum = sum + array1[i];
+    i++;
   }
-}
+  return sum;
+};
+
+console.log(funct(array1));
+

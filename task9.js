551e691f6d2f1df4f6a544f181b33d884ae91794
Denis <denis911p@gmail.com>
2023-01-18 20:59:51 +0300

    task9Finish

--- task9.js
+++ task9.js
@@ -1,8 +1,34 @@
-const array =[
-  { name: "Ivan", age: 24},
-  { name: "Oleg", age: 16},
-  { name: "Igor", age: 24}];
-array.sort(( a, b ) => a.age - b.age);
 
-console.log();
+
+//Решение №1
+// const array = [
+//   {name: "Ivan", age: 24},
+//   {name: "Oleg", age: 16},
+//   {name: "Igor", age: 24}];
+//
+// const obj = {min: array[0].age, max: array[0].age}
+// array.forEach((item) => {
+//   if (item.age > obj.max) {
+//     obj.max = item.age
+//   }
+//   if (item.age < obj.min) {
+//     obj.min = item.age
+//   }
+// });
+//
+// console.log(obj);
+
+
+
+
+
+const array = [
+  {name: "Ivan", age: 24},
+  {name: "Oleg", age: 16},
+  {name: "Igor", age: 24}];
+
+const arr = array.sort((a,b) => a.age - b.age)
+const obj = { min: arr[0].age, max: arr[array.length-1].age }
+
+console.log(obj);
 

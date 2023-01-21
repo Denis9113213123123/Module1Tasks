9f749d942215cc2a92585b86f640b0231da1e6cc
Denis <denis911p@gmail.com>
2023-01-21 13:40:21 +0300

    task11finish

--- task11.js
+++ task11.js
@@ -1,9 +1,11 @@
-const M = 10, N = 10;
-    const arr=[];
-for (let k = 0; k <= N; k++) {
-for (let i = 0; i <= M; i++) {
-  arr.push(k*i);
-};
-};
-console.log(arr);
+const arr = [];
+for (let k = 0; k <= 10; k++) {
+  let arr2 = [];
+  for (let i = 0; i <= 10; i++) {
+    arr2.push(k * i);
+  }
+  arr.push(arr2);
+}
+;
+console.table(arr);
 

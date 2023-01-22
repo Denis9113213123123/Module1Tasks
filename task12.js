14530abb782c9aa216ef1e5116f8dc11623249d8
Denis <denis911p@gmail.com>
2023-01-22 15:24:10 +0300

    task12finish

--- task12.js
+++ task12.js
@@ -1,25 +1,13 @@
-const M = 5, N = 2;
-const array = [];
-for (let i = 1; i <= N; i++) {
-array[i]=[];
-  for (let k = 0; k <= M; k++) {
+let j = 1;
+const arr = [];
+for (let k = 1; k <= 10; k++) {
+  let arr2 = [];
+  for (let i = 1; i <= 10; i++) {
+    arr2.push(j * j);
+    j++;
 
-
-    array[i][k] = k * k;
   }
-  ;
+  arr.push(arr2);
 }
 ;
-
-console.log(array);
-
-//
-// let n = 5, m = 4,l=100;
-// let mas = [];
-//
-// for (let i = 0; i < m; i++){
-//   mas[i] = [];
-//   for (let j = 0; j < n; j++){
-//     mas[i][j] = 1;
-//   }}
-// console.log(mas);
+console.table(arr);

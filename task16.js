455a2f69549042ee056479998cdbca3fb56e882e
Denis <denis911p@gmail.com>
2023-01-23 20:54:54 +0300

    task16fixconstandreturn

--- task16.js
+++ task16.js
@@ -1,7 +1,6 @@
 const num = "88005553535";
-funct = (num) => {
-  let str = num.slice(0, 1) + " (" + num.slice(1, 4) + ") " + num.slice(4, 7) + "-" + num.slice(7, 9) + "-" + num.slice(9);
-  console.log(str);
+const funct = (num) => {
+  return num.slice(0, 1) + " (" + num.slice(1, 4) + ") " + num.slice(4, 7) + "-" + num.slice(7, 9) + "-" + num.slice(9);
 };
 
-funct(num);
+console.log(funct(num));

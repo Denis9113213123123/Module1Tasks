79b0959b3b84581092a1a84382533fa866caa379
Denis <denis911p@gmail.com>
2023-01-23 21:53:47 +0300

    task17fixallmassiv

--- task17.js
+++ task17.js
@@ -1,7 +1,8 @@
 const array1 = [1,2,3];
 const array2 = [4,5,6];
 
-funct = (ar1,ar2) => {
-  console.log( [...ar1,...ar2]);
+const funct = (...args) => {
+  const arr = [];
+return arr.concat(...args);
 };
 console.log(funct(array1,array2))

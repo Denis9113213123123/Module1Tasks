d8a041ec9d19d64c2ee1d7a9929b3227a45e3a1f
Denis <denis911p@gmail.com>
2023-01-29 15:41:27 +0300

    task30fix

---
+++ task30.js
@@ -0,0 +1,13 @@
+const obj1 = {
+  id: 123,
+  first_name: "Ivan",
+  last_name: "Ivanov",
+  password: "Mypwd!23",
+  age: 13
+};
+const funct = (obj) => {
+  let clone = Object.assign({}, obj );
+  delete clone.password;
+  return clone;
+};
+console.log(funct(obj1))
